57100bab6ae5a22c323e8fff4f02836baee36e46
Jhody Lagarde <jhody.lagarde@outlook.fr>
2024-09-27 11:16:53 +0200

    debug php

--- api/form.php
+++ api/form.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $jsonData = file_get_contents("php://input");
     $data = json_decode($jsonData, true);
